e7556b2c287822058cb35330f64b96b57436b2fc
yu900701 <kd123456525@gmail.com>
2026-05-07 22:45:48 +0800

    modify test_api.py to simulate fail

--- tests/test_api.py
+++ tests/test_api.py
@@ -11,5 +11,6 @@ def test_root():
 def test_health():
     response = client.get("/health")
 
-    assert response.status_code == 200
+    # assert response.status_code == 200
+    assert 1 == 2
     assert response.json()["status"] == "healthy"
