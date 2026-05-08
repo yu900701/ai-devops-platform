113c393972a84d875244fc623732a08accc4e093
yu900701 <kd123456525@gmail.com>
2026-05-08 17:08:08 +0800

    update app version and seperate repo app and gitops repo

--- app/main.py
+++ app/main.py
@@ -23,6 +23,6 @@ def health_check():
 def info():
     return {
         "app": "AI DevOps Platform",
-        "version": "0.1.0",
+        "version": "0.2.0",
         "service": "FastAPI Backend"
     }
